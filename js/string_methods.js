68fccefabc40dc970814f0c032e4e7993bbeb744
MRosales0390 <Marco_Rosales@hotmail.es>
2022-05-19 20:15:56 -0500

    Added a couple of exercises to string_methods.js file

--- js/string_methods.js
+++ js/string_methods.js
@@ -7,3 +7,34 @@
 // .substring(start, end)
 // .slice(start, end)
 // .includes(searchElement, fromIndex)
+
+const p =
+  "El ágil zorro café salta sobre el perro perezoso. Sí el perro reaccionó, realmente es perezoso?";
+
+console.log(p.split("."));
+//Cadena dividida por punto
+
+console.log(p.split(" "));
+//Cadena dividia por espacio
+
+console.log("Cadena original:\n", p);
+
+let pos = 5;
+let char = p.charAt(pos);
+console.log(`El caracter ${char} está en la posición ${pos}`);
+
+//startsWith
+let testValue = "El agil";
+console.log(
+  `El siguiente texto "${p}" \n${
+    p.startsWith(testValue) ? "Sí" : "No"
+  } empieza con "${testValue}"`
+);
+
+//indexOf()
+let lookupValue = "tortuga";
+console.log(
+  `La palabra ${lookupValue} ${
+    p.includes(lookupValue) ? "sí" : "no"
+  } se encuentra en el texto.`
+);
