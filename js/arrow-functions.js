1faddb9f4458a9efa66565bdbfcb34c66e493274
MRosales0390 <Marco_Rosales@hotmail.es>
2022-05-24 23:28:28 -0500

    Added homework solution and arrow function examples

---
+++ js/arrow-functions.js
@@ -0,0 +1,152 @@
+//arrow functions -> funciones flecha
+/**
+ * 1. Declaración e invación de funciones
+ * 2. Funciones con parámetros
+ * 3. Funciones con parámetros opcionales o valores por defecto
+ * 4. Funciones de retorno
+ */
+
+// const strReverse = (str = "koder") => {
+//   let strReversed = "";
+
+//   for (let i = str.length - 1; i >= 0; i++) {
+//     strReversed += str[i];
+//   }
+
+//   return strReversed;
+// };
+
+/**
+ * Funcion que pida una palabra al usuario
+ * Invierta la palabra
+ * y la retorne en un alert
+ * reversedString('hola') -> 'aloh'
+ */
+let reversedString = (word) => {
+  let result = "";
+  let wordLength = word.length;
+  if (wordLength > 0) {
+    for (let i = wordLength - 1; i >= 0; i--) {
+      result += word[i];
+    }
+  } else {
+    result = "El dato introducido es inválido";
+  }
+
+  return result;
+};
+
+/**
+ * Funcion que pida 3 numeros como parametro
+ * 2 numeros obligatorios y 1 opcional con valor de 3
+ * Arroje la suma de esos 3 numeros
+ * Posibles resultados
+ * -> addThreeNumbers(3,4,5) -> 12
+ * -> addThreeNumbers(3,4) -> 10
+ * -> addThreeNumbers(3) -> 'Faltan datos'
+ * -> addThreeNumbers() -> 'Faltan datos'
+ */
+
+const addThreeNumbers = (firstNumber = 3, secondNumber, thirdNumber) => {
+  let result;
+
+  if (firstNumber >= 0 && (secondNumber >= 0 || thirdNumber >= 0)) {
+    result = 0;
+
+    if (firstNumber >= 0) result += firstNumber;
+
+    if (secondNumber >= 0) result += secondNumber;
+
+    if (thirdNumber >= 0) result += thirdNumber;
+  } else {
+    result = "Faltan datos.";
+  }
+
+  return result;
+};
+
+/**
+ * Funcion que pida al usuario los grados Centigrados en su localidad
+ * Convertirlos a grados fahrenheit
+ * Arrojar un alert con el resultado
+ * -> convertTemp(25) -> La temperatura es de 77 ºF
+ * -> convertTemp() -> 'Faltan datos'
+ */
+let convertTemp = (degrees) => {
+  let result;
+
+  if (degrees >= 0) {
+    result = `La temperatura es de ${degrees * (9 / 5) + 32} ºF`;
+  } else {
+    result = "Faltan datos.";
+  }
+
+  return result;
+};
+
+/**
+ * Funcion que imprima las tablas del 1 al 10
+ */
+let timesTable = (number) => {
+  if (number >= 1 && number <= 10) {
+    console.log(`Tabla de multiplicar del ${number}`);
+    for (let j = 1; j <= 10; j++) {
+      console.log(`${number} x ${j} = ${number * j}`);
+    }
+  } else if (number < 1 || number > 10) {
+    console.log("El valor indicado está fuera del rango.");
+  } else {
+    for (let i = 1; i <= 10; i++) {
+      console.log(`Tabla de multiplicar del ${i}`);
+      for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+      }
+    }
+  }
+};
+
+/**
+ * Funcion que pida al usuario un numero (N) entre 1 y 100
+ * Mandar un alert con suma de 1 a N
+ * Valor por defecto 3
+ * addNumberLimit() -> 6
+ * addNumberLimit(4) -> 10
+ * addNumberLimit(5) -> 15
+ */
+let addNumberLimit = (number = 3) => {
+  let result;
+  let counter = 1;
+
+  if (number >= 1 && number <= 100) {
+    result = 0;
+
+    while (counter <= number) {
+      result += counter;
+
+      counter++;
+    }
+  } else {
+    result = "Valor inválido.";
+  }
+
+  return result;
+};
+
+// Arrow function
+// Pedir al usuario una cadena de texto
+// Verificar si esa oración es un palíndromo
+const isPalindrome = (str) => {
+  //sentencias
+  let strWithouthSpaces = str.toLowerCase().replaceAll(" ", "");
+  let isThisAPalindrome = true;
+  let strSize = strWithouthSpaces.length - 1;
+
+  for (let i = strSize; i >= 0; i--) {
+    if (strWithouthSpaces[i] !== strWithouthSpaces[strSize - i]) {
+      isThisAPalindrome = false;
+      break;
+    }
+  }
+
+  return isThisAPalindrome;
+};
--- js/homework.js
+++ js/homework.js
@@ -1,113 +1,58 @@
 /**
- * Funcion que pida una palabra al usuario
- * Invierta la palabra
- * y la retorne en un alert
- * reversedString('hola') -> 'aloh'
+ * Ejercicio 1 *Obligatorio
+ * Realizar una funcion que reciba como parametro 1 array
+ * Y devuelva un array con solo los elementos Pares de ese array
+ * p.ej.
+ * -> evenOnly( [1,2,3,4,18] ) -> [2,4,18]
+ * -> evenOnly( [10, 2, 5] ) -> [10,2]
+ * -> evenOnly() -> 'Se necesita un array'
+ *
  */
-function reversedString(word) {
-  let result = "";
-  let wordLength = word.length;
-  if (wordLength > 0) {
-    for (let i = wordLength - 1; i >= 0; i--) {
-      result += word[i];
-    }
-  } else {
-    result = "El dato introducido es inválido";
-  }
+evenOnly = (numbersArray) => {
+  let onlyEvenNumbers = [];
 
-  return result;
-}
-/**
- * Funcion que pida 3 numeros como parametro
- * 2 numeros obligatorios y 1 opcional con valor de 3
- * Arroje la suma de esos 3 numeros
- * Posibles resultados
- * -> addThreeNumbers(3,4,5) -> 12
- * -> addThreeNumbers(3,4) -> 10
- * -> addThreeNumbers(3) -> 'Faltan datos'
- * -> addThreeNumbers() -> 'Faltan datos'
- */
-function addThreeNumbers(firstNumber = 3, secondNumber, thirdNumber) {
-  let result;
+  if (numbersArray) {
+    let arrayLenght = numbersArray.length;
 
-  if (firstNumber >= 0 && (secondNumber >= 0 || thirdNumber >= 0)) {
-    result = 0;
-
-    if (firstNumber >= 0) result += firstNumber;
-
-    if (secondNumber >= 0) result += secondNumber;
-
-    if (thirdNumber >= 0) result += thirdNumber;
-  } else {
-    result = "Faltan datos.";
-  }
-
-  return result;
-}
-
-/**
- * Funcion que pida al usuario los grados Centigrados en su localidad
- * Convertirlos a grados fahrenheit
- * Arrojar un alert con el resultado
- * -> convertTemp(25) -> La temperatura es de 77 ºF
- * -> convertTemp() -> 'Faltan datos'
- */
-function convertTemp(degrees) {
-  let result;
-
-  if (degrees >= 0) {
-    result = `La temperatura es de ${degrees * (9 / 5) + 32} ºF`;
-  } else {
-    result = "Faltan datos.";
-  }
-
-  return result;
-}
-
-/**
- * Funcion que imprima las tablas del 1 al 10
- */
-function timesTable(number) {
-  if (number >= 1 && number <= 10) {
-    console.log(`Tabla de multiplicar del ${number}`);
-    for (let j = 1; j <= 10; j++) {
-      console.log(`${number} x ${j} = ${number * j}`);
-    }
-  } else if (number < 1 || number > 10) {
-    console.log("El valor indicado está fuera del rango.");
-  } else {
-    for (let i = 1; i <= 10; i++) {
-      console.log(`Tabla de multiplicar del ${i}`);
-      for (let j = 1; j <= 10; j++) {
-        console.log(`${i} x ${j} = ${i * j}`);
+    for (let i = 0; i < arrayLenght; i++) {
+      if (numbersArray[i] % 2 === 0) {
+        onlyEvenNumbers.push(numbersArray[i]);
       }
     }
+
+    return onlyEvenNumbers;
+  } else {
+    return "Se necesita un array";
   }
-}
+};
 
 /**
- * Funcion que pida al usuario un numero (N) entre 1 y 100
- * Mandar un alert con suma de 1 a N
- * Valor por defecto 3
- * addNumberLimit() -> 6
- * addNumberLimit(4) -> 10
- * addNumberLimit(5) -> 15
+ * Ejercicio 2 *Opcional
+ * Funcion que reciba como parametro una array de strings
+ * y devuelva el primer y ultimo caracter de cada string
+ * p.ej.
+ * -> firstAndLast ( ['hola', 'mundo'] ) -> ['ha', 'mo']
  */
-function addNumberLimit(number = 3) {
-  let result;
-  let counter = 1;
+firstAndLast = (strArray) => {
+  let lettersArray = [];
 
-  if (number >= 1 && number <= 100) {
-    result = 0;
+  if (strArray) {
+    let strArraySize = strArray.length;
 
-    while (counter <= number) {
-      result += counter;
-
-      counter++;
+    for (let i = 0; i < strArraySize; i++) {
+      lettersArray.push(strArray[i].substring(0, 1) + strArray[i].slice(-1));
     }
-  } else {
-    result = "Valor inválido.";
-  }
 
-  return result;
-}
+    return lettersArray;
+  } else {
+    return "Se necesita un array";
+  }
+};
+
+/**
+ * Ejercicio 3
+ * Estudiar todos los métodos de Array
+ * https://www.w3schools.com/jsref/jsref_obj_array.asp
+ * https://www.programiz.com/javascript/library/array
+ *
+ */
