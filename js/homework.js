17a48eff589dd06c4455efe27725e9d1311ba7b6
MRosales0390 <Marco_Rosales@hotmail.es>
2022-05-30 16:51:48 -0500

    Added homework solution

--- js/homework.js
+++ js/homework.js
@@ -1,121 +1,152 @@
-/**
- * Funcion que reciba una palabra
- * Retorne la palabra invertida
- *
- * reverseStr('hola mundo')
- * -> 'odnum aloh'
- *
- * Usar solo metodos de str y array
- */
-const reverseStr = (phrase) => {
-  if (phrase) {
-    let strArray = phrase.split("");
-    let reversedArray = strArray.reverse();
-    let reversedStr = reversedArray.join("");
+// Ejercicios Obligatorios
 
-    return reversedStr;
-  } else {
-    return "Valor inválido";
-  }
+/**
+ * Dado el sigueinte arreglo de ciudades
+ *   ['méXicO', 'PErÚ', 'eSpAña', 'inGlaterrA']
+ * Generar una funcion que capitalize cada elemento del array
+ * => Se puede resolver con .forEach() o .map()
+ */
+let cities = ["méXicO", "PErÚ", "eSpAña", "inGlaterrA"];
+
+const capitilize = (cities) => {
+  return cities.map((city) => {
+    return city.charAt(0).toUpperCase() + city.slice(1).toLowerCase();
+  });
 };
 
-let phrase = "Hola mundo";
-
-console.log(reverseStr(phrase));
-
+console.log(
+  `Arreglo original: ${cities}; \nNuevo arreglo: ${capitilize(cities)}`
+);
 /**
- * Funcion que reciba un array de numeros
- * y devuelva el promedio de todos los elementos
+ * Dado un arreglo con nombres de personas,
+ * Funcion que devuele un arreglo con los nombres de las personas que empiezan con vocales y además capitalizados
  *
- * getAverage( [10, 8, 9, 7] )
- * -> 8.5
- */
-const getAverage = (numbersArray) => {
-  let sum = 0;
-
-  if (numbersArray) {
-    let arraySize = numbersArray.length;
-
-    numbersArray.forEach((number) => (sum += number));
-    sum /= arraySize;
-  }
-
-  return sum;
-};
-
-let numArray = [10, 8, 9, 7];
-
-console.log(getAverage());
-/**
- * Funcion que reciba un array de strings
- * y devuelva un array indicando si son palindromos o no
+ * onlyNamesVowels( ['jorge','ana','ivan','sergio','oscar' ] )
+ * -> ['Ana','Ivan','Oscar']
  *
- * arePalindrome( ['oso','juan', 'seres'] )
- * -> [true, false, true]
+ * => Se puede resolver con .forEach() o .filter()
  */
-const arePalindrome = (stringArray) => {
-  if (stringArray) {
-    let arePalindromes = stringArray.map((word) => {
-      word = word.replaceAll(" ", "");
-      let splittedWord = word.split("");
-      let reversedWord = splittedWord.reverse().join("");
+let namesList = ["jorge", "ana", "ivan", "sergio", "oscar"];
 
-      for (let i = 0; i < word.length; i++) {
-        if (word[i] !== reversedWord[i]) return false;
-      }
+const onlyNamesVowels = (names) => {
+  let vowels = "aeiou";
+  let capitalizedNamesList = [];
 
-      return true;
+  capitalizedNamesList = names
+    .filter((name) => {
+      if (vowels.includes(name.charAt(0).toLowerCase())) {
+        return true;
+      } else return false;
+    })
+    .map((name) => {
+      return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
     });
 
-    return arePalindromes;
-  } else {
-    return "Valor inválido";
-  }
+  return capitalizedNamesList;
 };
 
-let palindromesArray = ["oso", "juan", "seres"];
-
-console.log(arePalindrome(palindromesArray));
+console.log(onlyNamesVowels(namesList));
 
 /**
- * Estudiar los métodos de array
- * .filter()
- * .reduce()
+ *
+ * Dado una arreglo compuesto por arreglos,
+ * crea una funcion que calcule la suma de los arreglos
+ *
+ * additionMultiArr(  [	[1,2,3] , [1,3,2] , [3,2,1] ] )
+ * -> 18
+ */
+let numbersMultiArray = [
+  [1, 2, 3],
+  [1, 3, 2],
+  [3, 2, 1],
+];
+
+const additionMultiArr = (arregloDeArreglos) => {
+  let counter = 0;
+  arregloDeArreglos.forEach((arreglo) => {
+    arreglo.forEach((numero) => {
+      counter += numero;
+    });
+  });
+
+  return counter;
+};
+
+console.log(additionMultiArr(numbersMultiArray));
+
+/**
+ *
+ * Estudiar el  metodo reduce
+ * https://www.digitalocean.com/community/tutorials/js-finally-understand-reduce
+ * https://dataquarium.io/little-bits-reduce/
+ * https://www.programiz.com/javascript/library/array/reduce
  */
 
 /**
- *  array.filter()
- *  returns a new array
+ *
+ * Estudiar el tema de Objetos
+ * https://www.programiz.com/javascript/object
+ * https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object
+ * https://www.freecodecamp.org/espanol/news/javascript-crear-objecto-como-definir-objetos-en-js/
+ * https://www.w3schools.com/js/js_objects.asp
+ *
  */
-console.log("**** Array.filter() ****");
-const checkEven = (number) => {
-  if (number % 2 == 0) return true;
-  else return false;
-};
 
-let numbersArrayFilter = [1, 2, 3, 4, 18];
-let evenNumbers = numbersArrayFilter.filter(checkEven);
-
-console.log(evenNumbers);
+// Ejercicios opcionales
 
 /**
- *  array.reduce()
- *  returns a new array
- */
-console.log("**** Array.reduce() ****");
-const getAverageWithReduce = (numbersArray) => {
-  let sum = 0;
+* 
+  En un array con pedidos de un restaurant
+  la estructura por cada pedido es:
+  ['entrada','principal','postre','precio'],
 
-  if (numbersArray) {
-    let arraySize = numbersArray.length;
+  let pedidos = [
+      ['ensalada de pepinos','paella','platano', 100'],
+      ['ensalada de tomates','pescado','helado', 120],
+      ['ensalada simple','paella','yogurt', 80],
+      ['ensalada simple','enchiladas','yogurt', 80],
+      ['ensalada cesar','salmón','platano', 100],
+  ]
 
-    sum =
-      numbersArray.reduce((accumulator, number) => accumulator + number) /
-      arraySize;
-  }
+  Crear: 
 
-  return sum;
+  Funcion que devuelve el total de pedidos que tienen como plato principal "paella"
+  => Se puede resolver con .forEach() o .filter()
+
+  Funcion que devuelve los pedidos que tienen como postre "platano" o "helado" 
+  => Se puede resolver con .forEach() o .filter()
+
+  Funcion que devuelve los pedidos que tienen un precio superior a 90 
+  => Se puede resolver con .forEach() o .filter()
+  
+*/
+
+let pedidos = [
+  ["ensalada de pepinos", "paella", "platano", 100],
+  ["ensalada de tomates", "pescado", "helado", 120],
+  ["ensalada simple", "paella", "yogurt", 80],
+  ["ensalada simple", "enchiladas", "yogurt", 80],
+  ["ensalada cesar", "salmón", "platano", 100],
+];
+
+const ordersFilter = (orders, filterField, filterValue) => {
+  return orders.filter((order) => {
+    if (filterField === "mainCourse" && order[1] === filterValue) return true;
+    else if (filterField === "desert" && filterValue.includes(order[2]))
+      return true;
+    else if (filterField === "price" && order[3] > filterValue) return true;
+    else return false;
+  });
 };
 
-let numArrayForReduce = [10, 8, 9, 7];
-console.log(getAverageWithReduce(numArrayForReduce));
+console.log(ordersFilter(pedidos, "mainCourse", "paella"));
+console.log(ordersFilter(pedidos, "desert", "platano|helado"));
+console.log(ordersFilter(pedidos, "price", "90"));
+
+let ingredientes = ["chile", "jitomate", "cebolla", "ajo"];
+
+const asar = (ingrediente) => {
+  return ingrediente + "Asado";
+};
+ingredientes.map(asar);
+//-> [chileCocido, jitomateCocido, cebollaCocida, ajoCocido]
